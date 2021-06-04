e04244a3d239f49f60b09081d5268c8b26014848
Begimay <kbegimaj8@gmail.com>
2021-06-04 15:02:37 +0600

    удалила комментарии и исправила mood

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-// создание телеграмм бота
 const  { Telegraf }  =  require ( 'telegraf' )
-const bot = new Telegraf('1850273515:AAEnjzSN92LEOHPG0_zbD-fjfJK9iE9SC-M') //у каждого свои цифры, нельзя делиться 
+const bot = new Telegraf(' ')  
 
 bot.start(ctx => ctx.reply('Привет, хозяин '))
 
@@ -8,17 +7,15 @@ const hellos = [
     'привет', 'Привет', 'Здравствуй', 'Здравствуйте'
 ]
 
-const moods = [ //создали массив
-    'Отлично', 'Пойдет', 'Хорошо', 'Плохо', 'Великолепно'
+const moods = [ 
+    'Отлично', 'Пойдет', 'Хорошо'
 ]
 
 bot.hears(hellos, ctx => ctx.reply(ctx.message.text))
 
 bot.hears('Как дела?', ctx => ctx.reply(
-    moods[Math.floor(Math.random() * moods.length)] //необязательно писать внутри реплай, открываем {} после стрелки, пишем условия и тд, потом пишем ctx.replay('хорошо')
-)) // реплай отвечает
-
-bot.hears('') // пишем для того чтобы вводить в тг (слушает)
-
-console.log('BOT HAS BEEN STARTED')
-bot.launch() // для вывода 
+    moods[Math.floor(Math.random() * moods.length)] 
+)) 
+bot.hears('') 
+console.log('Stop')
+bot.launch() 
